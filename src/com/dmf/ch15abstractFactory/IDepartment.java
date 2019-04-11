ab57d9aa5e108f46a079471554402e273077ad2f
dengmengfei <dengmengfei@ysstech.com>
2019-04-11 09:53:44 +0800

    1

--- src/com/dmf/ch15abstractFactory/IDepartment.java
+++
@@ -1,8 +0,0 @@
-package com.dmf.ch15abstractFactory;
-
-public interface IDepartment {
-	
-	public void Insert(Department department);
-	public Department GetDepartment(int id);
-
-}
--- src/com/dmf/ch15abstractFactory/IUser.java
+++
@@ -1,8 +0,0 @@
-package com.dmf.ch15abstractFactory;
-
-public interface IUser {
-	
-	public void Insert(User user);
-	public User GetUser(int id);
-
-}
